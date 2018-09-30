7ea292844409e6bba15b42daad06b5165d26eb59
Sérgio Serra <sergioserra99@gmail.com>
2018-09-30 17:43:27 +0100

    Add Bulldog annotation. Fix module dependencies.

---
+++ app/src/main/java/com/criations/bulldog/UserSettings.kt
@@ -0,0 +1,11 @@
+package com.criations.bulldog
+
+import com.criations.bulldog_annotations.Bulldog
+
+
+/**
+ * @author Sérgio Serra on 30/09/2018.
+ * sergioserra99@gmail.com
+ */
+@Bulldog
+interface UserSettings {}
---
+++ bulldog-runtime/src/main/java/com/criations/bulldog_runtime/Bulldog.kt
@@ -0,0 +1,7 @@
+@file:Suppress("PackageName")
+
+package com.criations.bulldog_runtime
+
+interface Bulldog {
+    fun clearAll()
+}
