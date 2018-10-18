7b97013d4f5c6ad5f30acc7123b957bfd52f2273
Sérgio Serra <sergio.serra@olx.com>
2018-10-18 14:32:23 +0100

    Remove unused import.

--- app/src/main/java/com/criations/bulldog/MainActivity.kt
+++ app/src/main/java/com/criations/bulldog/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.criations.bulldog
 
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.criations.bulldog.UserModel.role
-import com.criations.bulldog.UserSettings2.*
 import com.criations.bulldog_runtime.bullDogCtx
 
 class MainActivity : AppCompatActivity() {
